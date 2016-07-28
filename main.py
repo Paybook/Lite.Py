0911296c7c2eef69748cfab1dd94026c11dda4c5
Mateo <donmikebautista@gmail.com>
2016-07-28 12:53:06 -0500

    commit test-4

--- main.py
+++ main.py
@@ -25,8 +25,6 @@ cmd_params = _Utilities.get_cmd_params(sys.argv[1:])
 paybook_api_key = cmd_params['api_key']
 # database_path = cmd_params['database']
 
-# Test
-
 paybook_sdk.Paybook(paybook_api_key)
 print '\nSetting API Key to: ' + str(paybook_api_key)
 print 'Server started successfully\n'
